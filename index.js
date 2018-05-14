4890f1e2019f9eaecd75770763afbc8c334ac2aa
jneilon <justin@jneilon.com>
2018-05-14 13:23:59 -0400

    wheres my cookies?

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 var express = require('express');
-
-
 const app = express();
-
-// This is for the public folder on path /
-app.use(express.static('public'));
-
-// this is for images folder on path images
-app.use('/images', express.static('images'));
-
 app.get('/', (req, res) =>
     res.send('duder'));
 
