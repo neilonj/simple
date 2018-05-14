844a857a8ed9f3b06973e0e5ceccecebef174826
jneilon <justin@jneilon.com>
2018-05-14 11:27:35 -0400

    data

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+//hotdog
+var jdata = require('data.json');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
