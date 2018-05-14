50b8dbe74e5dac785c2e7b86d590ac02911cfc41
jneilon <justin@jneilon.com>
2018-05-14 11:15:11 -0400

    't25'

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var data = require('./data/data.json');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
