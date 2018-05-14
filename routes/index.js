c2d4404cdc28d14ab9db6a20296710c405c06d4a
jneilon <justin@jneilon.com>
2018-05-13 23:37:27 -0400

    't20'

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
-import data from "/data.json";
-
 var express = require('express');
 var router = express.Router();
+var data = require('./data.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
