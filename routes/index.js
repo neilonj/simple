fe8a6c6d97aaaacfea78734e26803f2149928e8d
jneilon <justin@jneilon.com>
2018-05-13 22:19:49 -0400

    TEST 5

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-import data from './data/MOCK_DATA.json';
+//import data from './data/MOCK_DATA.json';
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
