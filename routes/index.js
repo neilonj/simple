5907f1f27fc8ff4cbff11cec3fc7412252c23cd9
jneilon <justin@jneilon.com>
2018-05-13 20:52:16 -0400

    TEST 2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+import data from './data/data.json';
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.post('/newitem', (req, res) =>
+  res.send(`a post request with /newitem route`));
 module.exports = router;
