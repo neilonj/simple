9325a96afec9cecb096132f4f7a97be7d5547554
jneilon <justin@jneilon.com>
2018-05-13 23:19:50 -0400

    't16'

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 //import data from "/data.json";
-var express = require('express');
-var router = express.Router();
 const app = express();
+var express = require('express');
+var router = app.Router();
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 app.get('/new',(req, res) =>
-  res.send("WWOAH "));
+  res.send('WOAH'));
 module.exports = router;
